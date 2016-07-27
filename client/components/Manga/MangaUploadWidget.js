3568c0fd2cbff3293edb1fffe67edcf67e42158d
Seongjun Kim <isair.zun@gmail.com>
2016-07-28 06:54:37 +0900

    Upload widgit use html from

--- client/components/Manga/MangaUploadWidget.js
+++ client/components/Manga/MangaUploadWidget.js
@@ -7,47 +7,44 @@ import { characters, bookTypes } from 'constants/manga'
 import styles from './MangaUploadWidget.css'
 
 export class MangaUploadWidget extends Component {
-  addManga = () => {
-    const nameRef = this.refs.name
-    const titleRef = this.refs.title
-    const contentRef = this.refs.content
-    if (nameRef.value && titleRef.value && contentRef.value) {
-      this.props.dispatch(addMangaRequest({
-        name: nameRef.value,
-        title: titleRef.value,
-        content: contentRef.value,
-      }))
-      nameRef.value = titleRef.value = contentRef.value = ''
-    }
-  };
+  onSubmit = (e) => {
+    e.preventDefault()
+    const payload = new FormData(e.target)
+    console.log(payload)
+    this.props.dispatch(addMangaRequest(payload))
+  }
 
   render () {
     const { className, intl } = this.props
     return (
       <div className={className || ''}>
-        <div className={styles['form-content']}>
-          <input placeholder={intl.messages.title} className={styles['form-field']} ref='title' />
-          <input placeholder={intl.messages.author} className={styles['form-field']} ref='author' />
+        <form className={styles['form-content']} onSubmit={this.onSubmit}>
+          <input placeholder={intl.messages.title} className={styles['form-field']} name='title' />
+          <input placeholder={intl.messages.author} className={styles['form-field']} name='author' />
           <div className={styles['form-field']} >
             {bookTypes.map(name =>
               <span key={name} className={styles['form-checkbox']}>
-                <input type='radio' id={`type_${name}`} name='type' value={name} ref='type' />
-                <label htmlFor={`type_${name}`}><FormattedMessage id={name} /></label>
+                <label>
+                  <input type='radio' name='type' value={name} />
+                  <FormattedMessage id={name} />
+                </label>
               </span>
             )}
           </div>
           <div className={styles['form-field']} >
             {characters.map(name =>
               <span key={name} className={styles['form-checkbox']}>
-                <input type='checkbox' id={`characters_${name}`} name='characters' value={name} ref='characters' />
-                <label htmlFor={`characters_${name}`}><FormattedMessage id={name} /></label>
+                <label>
+                  <input type='checkbox' name='characters' value={name} />
+                  <FormattedMessage id={name} />
+                </label>
               </span>
             )}
           </div>
-          <input placeholder={intl.messages.file} className={styles['form-field']} type='file' ref='files' />
-          <textarea placeholder={intl.messages.postContent} className={styles['form-field']} ref='content' />
-          <a className={styles['post-submit-button']} href='#' onClick={this.addManga}><FormattedMessage id='submit' /></a>
-        </div>
+          <input placeholder={intl.messages.file} className={styles['form-field']} type='file' name='file' />
+          <textarea placeholder={intl.messages.postContent} className={styles['form-field']} name='content' />
+          <input className={styles['form-field']} type='submit'></input>
+        </form>
       </div>
     )
   }
