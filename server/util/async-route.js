a6232bb1d4c014849dc5641c65e320073ec89742
Seongjun Kim <isair.zun@gmail.com>
2016-07-28 07:07:57 +0900

    Response 500 error when api fail

--- server/util/async-route.js
+++ server/util/async-route.js
@@ -1,3 +1,7 @@
 export default function asyncRoute (asyncFunc) {
-  return (req, res, next) => asyncFunc(req, res, next).catch(next)
+  return (req, res, next) => asyncFunc(req, res, next)
+    .catch(err => {
+      console.err(err)
+      res.status(500).end()
+    })
 }
